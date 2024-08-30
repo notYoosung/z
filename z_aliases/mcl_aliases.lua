8cf15e82166423a2a77343472083a2b01899675e
notYoosung <notYoosung4901@gmail.com>
2024-08-29 20:51:34 -0500

    fix aliases

--- z_aliases/mcl_aliases.lua
+++ z_aliases/mcl_aliases.lua
@@ -1,8 +1,10 @@
 minetest.register_on_mods_loaded(function()
     for k, v in pairs(minetest.registered_nodes) do
-        if v:gmatch("^mcl_") then
-            minetest.register_alias("mcl:" .. v:match("mcl_.*:(.*)"), v)
-            minetest.register_alias(v:match("mcl_.*:(.*)"), v)
+        minetest.log(k)
+        -- print(k)
+        if k:match("^mcl_") then
+            minetest.register_alias("mcl:" .. k:match("mcl_.*:(.*)"), k)
+            minetest.register_alias(k:match("mcl_.*:(.*)"), k)
         end
     end
 end)
